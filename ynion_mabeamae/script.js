5f76a4159866ec342bb6951197c00366a9621aa1
ynion-mabeamae <mabeamaeynion@gmail.com>
2025-03-26 08:59:25 +0800

    Update all files. (#3)
    * Update all files.
    
    * Add indention to button tag.

--- ynion_mabeamae/script.js
+++ ynion_mabeamae/script.js
@@ -1,6 +1,7 @@
 let commentName = document.querySelector("#comment_name");
 let commentContent = document.querySelector("#comment_text");
 let commentButton = document.querySelector("#comment_button");
+let commentList = document.querySelector("#comment");
 
 function validateComment() {
   if (commentName.value && commentContent.value) {
@@ -10,8 +11,24 @@ function validateComment() {
   }
 }
 
+function addComment() {
+  let name = commentName.value;
+  let comment = commentContent.value;
+
+  if (name && comment) {
+    const newComment = `<div class="upper-tags">
+                          <div class="user-tag"><strong>${name}:</strong></div>
+                          <div class="user-comment">${comment}</div>
+                        </div>`;
+
+    commentList.insertAdjacentHTML("beforeend", newComment);
+
+    commentName.value = "";
+    commentContent.value = "";
+    commentButton.disabled = true;
+  }
+}
+
 commentName.addEventListener("input", validateComment);
 commentContent.addEventListener("input", validateComment);
 commentButton.addEventListener("click", addComment);
-
-validateComment();
