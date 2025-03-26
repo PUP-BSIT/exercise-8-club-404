6987dc0b67a54f2f4016d168d52651910e1c7ebe
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-03-26 11:58:53 +0800

    add function to display comment

--- arroyo_johnmatthew/script.js
+++ arroyo_johnmatthew/script.js
@@ -8,3 +8,21 @@ function buttonEnabler() {
 		document.getElementById("comment_button").disabled = true;
 	}
 }
+
+function displayComment() {
+	let commentContainer = document.querySelector("#comment_container");
+	let commenter = document.getElementById("user_name");
+	let comment = document.getElementById("user_comment");
+	let button = document.getElementById("comment_button");
+	const newName = `<h2 class="commenter">${commenter.value}</h2>`;
+	const newComment = `<p class="comment-content">${comment.value}</p>`;
+
+	if(!button.disabled) {
+		commentContainer.insertAdjacentHTML("beforeend", newName);
+		commentContainer.insertAdjacentHTML("beforeend", newComment);
+	}
+
+	commenter.value = "";
+	comment.value = "";
+	button.disabled = true;
+}
